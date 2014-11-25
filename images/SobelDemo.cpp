f44177df72bd7b3fe9d57cd87075f8dabd4689cf
Salomon Lee <lsmon714@aol.com>
2014-11-25 00:25:54 -0800

    changing image path location

--- images/SobelDemo.cpp
+++ images/SobelDemo.cpp
@@ -5,7 +5,7 @@ int main(int argc, const char** argv)
 {
     cout << "Hello World!" << endl;
     cout << "Number of args: " << argc << endl;
-    string filename = "../../data/baboon.jpg";
+    string filename = "../data/baboon.jpg";
     if (argc < 2) {
         cout << "The image file will be set as " << filename << endl;
     } else {
